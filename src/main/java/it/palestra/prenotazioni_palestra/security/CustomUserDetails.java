b107225890494c4e14fcf37ff267ee5fe9a7ef56
francescodigiamb <francescodigiamb@gmail.com>
2025-11-20 00:30:26 +0100

    blocco login
    blocco login se utente non verificato

--- src/main/java/it/palestra/prenotazioni_palestra/security/CustomUserDetails.java
+++ src/main/java/it/palestra/prenotazioni_palestra/security/CustomUserDetails.java
@@ -48,7 +48,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return utente.isEnabled();
     }
 
     public Utente getDomainUser() {
